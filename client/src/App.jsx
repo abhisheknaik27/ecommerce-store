20524e5030d50fa7cd3104cfdf654f718954df9a
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-11-21 12:06:13 +0530

    shopping routes and layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,12 @@ import AdminDashboard from "./pages/admin-view/AdminDashboard"
 import AdminProducts from "./pages/admin-view/AdminProducts"
 import AdminOrders from "./pages/admin-view/AdminOrders"
 import AdminFeatures from "./pages/admin-view/AdminFeatures"
+import ShoppingLayout from "./components/shopping-view/ShoppingLayout"
+import NotFound from "./pages/not-found/NotFound"
+import Account from "./pages/shopping-view/Account"
+import ProductListing from "./pages/shopping-view/ProductListing"
+import ShopHome from "./pages/shopping-view/ShopHome"
+import Checkout from "./pages/shopping-view/Checkout"
 
 const App = () => {
   return (
@@ -15,16 +21,26 @@ const App = () => {
       <h1>HEADER</h1>
 
       <Routes>
+        {/* auth routes */}
         <Route path="/auth"  element={<AuthLayout />}>
           <Route path="login" element={<AuthLogin />} />
           <Route path="register" element={<AuthRegister />} />
         </Route>
+        {/* admin routes */}
         <Route path="/admin" element={<AdminLayout />}>
           <Route path="dashboard" element={<AdminDashboard />}/>
           <Route path="products" element={<AdminProducts />}/>
           <Route path="orders" element={<AdminOrders />}/>
           <Route path="features" element={<AdminFeatures />}/>
         </Route>
+        {/* shopping routes */}
+        <Route path="/shop" element={<ShoppingLayout />}>
+          <Route path="home" element={<ShopHome />} />
+          <Route path="listing" element={<ProductListing />} />
+          <Route path="account" element={<Account />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   )
---
+++ client/src/components/shopping-view/ShoppingHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ShoppingHeader = () => {
+  return (
+    <div>
+      ShoppingHeader
+    </div>
+  )
+}
+
+export default ShoppingHeader
+
